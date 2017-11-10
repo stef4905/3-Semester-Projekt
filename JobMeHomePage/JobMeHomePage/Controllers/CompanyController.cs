8721872c432fa03ddaa2a39ae4156bbe0a2fc214
Christian Trab <christianmtrab@gmail.com>
2017-11-10 13:42:21 +0100

    Partialview, Modal added for Opret brugere!

---
+++ JobMeHomePage/JobMeHomePage/Controllers/CompanyController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JobMeHomePage.Controllers
+{
+    public class CompanyController : Controller
+    {
+        // GET: Company
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult _CreateCompany()
+        {
+            return PartialView();
+        }   
+    }
+}
