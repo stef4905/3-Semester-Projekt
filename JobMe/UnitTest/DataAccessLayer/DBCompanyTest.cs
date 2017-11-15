934c20eaf8cb92c1e7a81f9c6a2bc47996667733
Mathias Væver Nikolajsen <1061741@ucn.dk>
2017-11-15 08:54:24 +0100

    Test til JobPOst

--- JobMe/UnitTest/DataAccessLayer/DBCompanyTest.cs
+++ JobMe/UnitTest/DataAccessLayer/DBCompanyTest.cs
@@ -22,34 +22,7 @@ namespace UnitTest.DataAccessLayer
             Assert.IsTrue(inserted);
         }
 
-        [TestMethod]
-        public void CompanyCreateJobPostDBTest()
-        {
-            //Arrange
-            DbCompany dbCompany = new DbCompany();
-            Company company = new Company("imacompany@gmai.om", "hellopassword");
-            company.Id = 1;
-            WorkHours workHours = new WorkHours();
-            workHours.Id = 1;
-            JobCategory jobCategory = new JobCategory();
-            jobCategory.Id = 1;
-
-
-            string iDate = "05/05/2018";
-            DateTime oDate = Convert.ToDateTime(iDate);
-            JobPost jobPost = new JobPost("Test job1", "Et job for dig", DateTime.Now, oDate, "Wc Vasker", workHours, "Her", company, jobCategory);
-
-
-
-            //Act
-            bool inserted = dbCompany.createJobPost(jobPost);
-
-            //Assert
-            Assert.IsTrue(inserted);
-        }
-
-
-
+       
 
     }
 }
