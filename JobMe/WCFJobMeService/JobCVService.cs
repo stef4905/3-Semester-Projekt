da6ec3165830838fba7efeaea42319d45c5b17e0
Steffen Nielsen <1061887@ucn.dk>
2017-11-17 13:14:04 +0100

    New service for JobCV has been created

---
+++ JobMe/WCFJobMeService/JobCVService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModelLayer;
+using BusinessLogicLayer;
+
+namespace WCFJobMeService
+{
+    public class JobCVService : IJobCVService
+    {
+        JobCVCtr jobCVCtr = new JobCVCtr();
+
+        public void Create(JobCV jobCV)
+        {
+            jobCVCtr.Create(jobCV);
+        }
+
+        public void Delete(int id)
+        {
+            jobCVCtr.Delete(id);
+        }
+
+        public JobCV Get(int applierId)
+        {
+            return jobCVCtr.Get(applierId);
+        }
+
+        public List<JobCV> GetAll()
+        {
+            return jobCVCtr.GetAll();
+        }
+
+        public void Update(JobCV obj)
+        {
+            jobCVCtr.Update(obj);
+        }
+    }
+}
