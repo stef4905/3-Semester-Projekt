ce11181e7a2aa55bd40f1dbaaf4a5c84f230c543
Steffen Nielsen <1061887@ucn.dk>
2017-11-20 11:00:12 +0100

    mo

--- JobMeHomePage/JobMeHomePage/Models/VMJobCVAndApplication.cs
+++ JobMeHomePage/JobMeHomePage/Models/VMJobCVAndApplication.cs
@@ -7,25 +7,7 @@ namespace JobMeHomePage.Models
 {
     public class VMJobCVAndApplication
     {
-        public JobMeHomePage.JobCVServiceReference.JobCV JobCV { get; set; }
-        public JobMeHomePage.ApplierServiceReference.Applier Applier { get; set; }
-
-        /// <summary>
-        /// Cunstroctor with the needed parameters
-        /// </summary>
-        /// <param name="jobCV"></param>
-        /// <param name="applier"></param>
-        public VMJobCVAndApplication(JobMeHomePage.JobCVServiceReference.JobCV jobCV, JobMeHomePage.ApplierServiceReference.Applier applier)
-        {
-            this.JobCV = jobCV;
-            this.Applier = applier;
-        }
-
-        /// <summary>
-        /// Empty constructor
-        /// </summary>
-        public VMJobCVAndApplication()
-        {
-        }
+            public JobMeHomePage.JobCVServiceReference.JobCV JobCV { get; set; }
+            public JobMeHomePage.ApplierServiceReference.Applier Applier { get; set; }
     }
 }
